84049ab55e4be6e504bced393c20c21e576bb12a
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-10-07 14:29:53 +0530

    adds to root page and custom path helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/privacy'
-  get 'pages/about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "pages#home"
+  get "privacy", to: "pages#privacy"
+  get "about", to: "pages#about"
 end
