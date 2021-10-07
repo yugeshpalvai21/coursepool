4d8061318a3a14912be1872a13080b1da2d3aada
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-10-07 14:15:30 +0530

    generates pages controller with basic actions

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def privacy
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/privacy'
+  get 'pages/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
