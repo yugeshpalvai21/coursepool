8e855b15461cff0dad6fe8d2d70a339e484e534f
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-10-19 08:09:23 +0530

    re-seeding User n Course data and modifies Courses index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,16 @@
+3.times do |n|
+  User.create({
+    email: "yugeshpalvai#{n}@coursepool.com", 
+    password: '123456', 
+    password_confirmation: '123456'
+  })
+end
+
+
 30.times do
   Course.create({
     title: FFaker::Book.title,
-    description: FFaker::DizzleIpsum.paragraphs
+    description: FFaker::DizzleIpsum.paragraphs,
+    user_id: rand(1..3)
   })
 end
