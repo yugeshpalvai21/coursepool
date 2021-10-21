2e3cd36dc9dffd1da96f182844986f6f7865ca80
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-10-21 09:41:13 +0530

    implements users index controller to display basic user data

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :courses
+  resources :users, only: [:index]
   root "pages#home"
   get "privacy", to: "pages#privacy"
   get "about", to: "pages#about"
