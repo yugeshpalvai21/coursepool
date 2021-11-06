3f5844f8ee492af890d5f140cae7e13ede250478
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-11-06 12:49:12 +0530

    temp changes to production mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -122,7 +122,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :user_name => "apikey", 
     :password => Rails.application.credentials.sendgrid_api_key, 
-    :domain => 'coursepool.herokuapp.com',
+    :domain => 'rubycommunity.org',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
