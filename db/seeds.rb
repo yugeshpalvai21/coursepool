1f135febc2286fb998f03e8e787774cdc5c7f746
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-11-06 20:35:19 +0530

    modifies view updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,8 @@
   User.create({
     email: "yugeshpalvai#{n}@coursepool.com", 
     password: '123456', 
-    password_confirmation: '123456'
+    password_confirmation: '123456',
+    confirmed_at: Time.now
   })
 end
 
@@ -11,6 +12,10 @@ end
   Course.create({
     title: FFaker::Book.title,
     description: FFaker::DizzleIpsum.paragraphs,
+    short_description: FFaker::Book.title,
+    price: rand(0..99),
+    level: ["beginner", "intermediate", "advanced"].sample,
+    language: ["english", "other"].sample,
     user_id: rand(1..3)
   })
 end
