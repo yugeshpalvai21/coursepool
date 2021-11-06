df3f44e3b620fbedc5023f60923513b9feda9131
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-11-06 16:50:15 +0530

    adds more validations to Course model and whitlisted atributes for courses controller

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ApplicationRecord
-  validates :title, presence: true
+  validates :title, :short_description, :price, presence: true
   validates :description, presence: true, length: { minimum: 5, maximum: 5000 }
+  validates :short_description, length: { minimum: 5, maximum: 160 }
 
   has_rich_text :description
 
