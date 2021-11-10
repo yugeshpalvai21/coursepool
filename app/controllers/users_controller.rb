a02221f97ab750d1e83b8103046a0e2ae4ebf40b
yugeshpalvai <yugeshpalvai21@gmail.com>
2021-11-10 09:49:14 +0530

    implements edit form to change users roles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,6 @@ class UsersController < ApplicationController
     end
 
     def user_params
-        params.require(:user).permit(roles_ids: [])
+        params.require(:user).permit(role_ids: [])
     end
 end
