731da50f511ba6d97c0eda4982f67285ce6ab9c4
yugeshpalvai <yugeshpalvai21@gmail.com>
2021-11-15 14:06:07 +0530

    update user record for every server request from signed_in user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  after_action :update_user_presence, if: :user_signed_in?
+
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   def user_not_authorized
     flash[:notice] = "Youre Not Allowed To Perform This Action"
     redirect_to root_path
+  end
+
+  def update_user_presence
+    current_user.try :touch
   end
 end
