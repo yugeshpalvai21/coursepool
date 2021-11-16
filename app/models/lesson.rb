272f9f7a7585c395509830fed46bcc1d9ae30aa9
yugeshpalvai <yugeshpalvai21@gmail.com>
2021-11-16 20:15:12 +0530

    Integrates friendly_id to Lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,6 @@
 class Lesson < ApplicationRecord
   belongs_to :course
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_102956) do
+ActiveRecord::Schema.define(version: 2021_11_16_144045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,7 +93,9 @@ ActiveRecord::Schema.define(version: 2021_11_16_102956) do
     t.bigint "course_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
     t.index ["course_id"], name: "index_lessons_on_course_id"
+    t.index ["slug"], name: "index_lessons_on_slug", unique: true
   end
 
   create_table "roles", force: :cascade do |t|
