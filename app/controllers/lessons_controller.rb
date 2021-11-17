17fe8aa580254f8d06c1c33ac6536a4ee3969a4d
yugeshpalvai <yugeshpalvai21@gmail.com>
2021-11-17 23:18:05 +0530

    adds nested routing for lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,10 @@
 class LessonsController < ApplicationController
+  before_action :set_course
   before_action :set_lesson, only: %i[ show edit update destroy ]
 
   # GET /lessons or /lessons.json
   def index
-    @lessons = Lesson.all
+    @lessons = @course.lessons
   end
 
   # GET /lessons/1 or /lessons/1.json
@@ -21,15 +22,13 @@ class LessonsController < ApplicationController
 
   # POST /lessons or /lessons.json
   def create
-    @lesson = Lesson.new(lesson_params)
+    @lesson = @course.lessons.new(lesson_params)
 
     respond_to do |format|
       if @lesson.save
-        format.html { redirect_to @lesson, notice: "Lesson was successfully created." }
-        format.json { render :show, status: :created, location: @lesson }
+        format.html { redirect_to course_path(@course), notice: "Lesson was successfully created." }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @lesson.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -38,11 +37,9 @@ class LessonsController < ApplicationController
   def update
     respond_to do |format|
       if @lesson.update(lesson_params)
-        format.html { redirect_to @lesson, notice: "Lesson was successfully updated." }
-        format.json { render :show, status: :ok, location: @lesson }
+        format.html { redirect_to course_path(@course), notice: "Lesson was successfully updated." }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @lesson.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -51,7 +48,7 @@ class LessonsController < ApplicationController
   def destroy
     @lesson.destroy
     respond_to do |format|
-      format.html { redirect_to lessons_url, notice: "Lesson was successfully destroyed." }
+      format.html { redirect_to course_path(@course), notice: "Lesson was successfully destroyed." }
       format.json { head :no_content }
     end
   end
@@ -65,5 +62,9 @@ class LessonsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def lesson_params
       params.require(:lesson).permit(:title, :content, :course_id)
+    end
+
+    def set_course
+      @course = Course.friendly.find(params[:course_id])
     end
 end
