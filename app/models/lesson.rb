0ea800be509532e5a627a98c3c8e898681c7dd69
yugeshpalvai <yugeshpalvai21@gmail.com>
2021-11-18 17:00:48 +0530

    adds basic validations to Lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,9 @@
 class Lesson < ApplicationRecord
   belongs_to :course
 
+  validates :title, presence: true, length: { minimum: 5, maximum: 80 }
+  validates :content, presence: true, length: { minimum: 10 }
+
   extend FriendlyId
   friendly_id :title, use: :slugged
 
