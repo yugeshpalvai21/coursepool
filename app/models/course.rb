bfd543138c4655b9505142266f281bab2eda54ba
yugeshpalvai <yugeshpalvai21@gmail.com>
2021-12-22 16:51:36 +0530

    implments association b/w course, user and enrollments

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,17 +1,16 @@
 class Course < ApplicationRecord
+  include PublicActivity::Model
+  tracked
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
   validates :title, :short_description, :price, presence: true
   validates :description, presence: true, length: { minimum: 5, maximum: 5000 }
   validates :short_description, length: { minimum: 5, maximum: 160 }
 
   has_rich_text :description
-
   belongs_to :user
-
   has_many :lessons, dependent: :destroy
-
-  extend FriendlyId
-  friendly_id :title, use: :slugged
-
-  include PublicActivity::Model
-  tracked
+  has_many :enrollments, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,21 @@
 class User < ApplicationRecord
   rolify
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  extend FriendlyId
+  friendly_id :email, use: :slugged
+
+  after_create :assign_default_role
+
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable, :trackable
 
   has_many :courses
-
-  after_create :assign_default_role
+  has_many :enrollments, dependent: :destroy
 
   def to_s
     email
   end
 
-  extend FriendlyId
-  friendly_id :email, use: :slugged
-  
   def assign_default_role
     self.add_role(:admin) if User.count == 1
     self.add_role(:teacher)
